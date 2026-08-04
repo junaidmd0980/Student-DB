f776dd1d5be8dc601762ff75a4c0077bbcd10e9d
Mohammad Junaid <junaidmd0980@gmail.com>
2026-08-04 20:37:14 +0530

    add env for cors origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ import labSessionRouter from "./routes/labSession.route.js";
 const app = express()
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: VITE_API_FRONTEND_URL, 
   credentials: false
 }));
 
