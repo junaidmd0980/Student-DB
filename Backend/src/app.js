1c4825f1e7728ceaaa098e734fd3f0d88fccbea4
Mohammad Junaid <junaidmd0980@gmail.com>
2026-08-05 18:19:50 +0530

    enable cors for testing locally

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,21 +1,24 @@
 import express from "express";
 import cors from "cors";
+import authRouter from "./routes/auth.route.js"
 import batchRouter from "./routes/batch.route.js";
 import departmentRouter from "./routes/depertment.route.js";
 import sectionRouter from "./routes/section.route.js";
 import studentRouter from "./routes/student.route.js";
 import labSessionRouter from "./routes/labSession.route.js";
+import { register } from "./controllers/auth.controller.js";
 
 
 const app = express()
 
 app.use(cors({
-  origin: [process.env.VITE_API_FRONTEND_URL, process.env.VITE_API_CUSTOM_DOMAIN], 
+  origin: [process.env.VITE_API_FRONTEND_URL, process.env.VITE_API_CUSTOM_DOMAIN, "localhost:5173"], 
   credentials: false
 }));
 
 app.use(express.json())
 
+app.use("/api/auth", authRouter);
 app.use("/api/departments", departmentRouter);
 app.use("/api/batches", batchRouter);
 app.use("/api/sections", sectionRouter);
