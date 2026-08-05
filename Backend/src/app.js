eb8c64c01b7ed0c371eb33310785905f7a404e80
Mohammad Junaid <junaidmd0980@gmail.com>
2026-08-05 09:39:11 +0530

    added custom domain

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ import labSessionRouter from "./routes/labSession.route.js";
 const app = express()
 
 app.use(cors({
-  origin: process.env.VITE_API_FRONTEND_URL, 
+  origin: [process.env.VITE_API_FRONTEND_URL, process.env.VITE_API_CUSTOM_DOMAIN], 
   credentials: false
 }));
 
