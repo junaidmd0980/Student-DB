4716044faca9d4777177b3dea9a98b7ca7bfd775
Mohammad Junaid <junaidmd0980@gmail.com>
2026-08-16 12:43:20 +0530

    updates variables

--- Frontend/src/shared/services/api.js
+++ Frontend/src/shared/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessToken } from "../../Auth/services/tokenStore";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL_LOCAL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
